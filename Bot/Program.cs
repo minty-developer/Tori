0bdbaf43fa4b9557aff5d9e8cb886b4bd543f934
minty-developer <gimminseung036@gmail.com>
2026-07-21 15:38:00 +0900

    v1.0.6
    서버 고치기 1트

--- Bot/Program.cs
+++ Bot/Program.cs
@@ -7,6 +7,12 @@ var builder = WebApplication.CreateBuilder(new WebApplicationOptions
     Args = args
 });
 
+builder.Configuration.Sources.Clear();
+builder.Configuration
+    .SetBasePath(AppContext.BaseDirectory)
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: false)
+    .AddEnvironmentVariables();
+
 // Discord.Net 소켓 클라이언트 설정 (게이트웨이 인텐트: 비특권 전체 + 메시지 본문 읽기 권한)
 builder.Services.AddSingleton(x => new DiscordSocketConfig
 {
